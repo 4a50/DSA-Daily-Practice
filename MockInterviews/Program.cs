6cfbddfa285353853be863754eaba3a8b9ef0b14
4a50 <jpjones22@gmail.com>
2021-04-26 16:27:41 -0700

    Added MockInterview question 4-26-21

---
+++ MockInterviews/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MockInterviews
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      //OddNumber
+      int[] arr = new int[] { 5, 1, 5, 1, 6, 5, 5 };
+      int oddNumber = OddNumber(arr);
+      Console.WriteLine("Integer Repeated Odd Number of Times: " + oddNumber);
+    }    
+      //# array of integers and one appears an odd number of times
+
+      //# ex. 4,6,4 = 6
+      //# ex. 1,2,3,4,1,3,4 = 2
+      //# ex. 3,3,3,4,4,4,3,4,5,5,5 = 5
+      //# ex. 5,1,5,1,6,5,5
+  static int OddNumber(int[] arr)
+    {
+      Dictionary<int, int> hashTable = new Dictionary<int, int>();
+      int dictValue = 0;
+      for (int i = 0; i < arr.Length; i++)
+      {
+        if (!hashTable.ContainsKey(arr[i])){ hashTable.Add(arr[i], 1); }
+        else { 
+            hashTable.TryGetValue(arr[i], out dictValue);
+          dictValue++;
+          hashTable[arr[i]] = dictValue;
+        }
+      }
+        foreach (KeyValuePair<int, int> val in hashTable)
+        {
+          if (val.Value % 2 != 0) return val.Key;
+        }
+      return 0;
+    }
+
+
+  }
+}
