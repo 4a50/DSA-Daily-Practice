a41aff81ab294a698af85e600875dc66da6d7351
4a50 <jpjones22@gmail.com>
2021-11-30 10:54:59 -0800

    Added Print to LL

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -8,7 +8,9 @@ namespace DataStructures
   {
     public Node<T> Head { get; set; }
     public Node<T> Tail { get; set; }
-    public LinkedList() { }
+    public LinkedList() {
+      Head = null;
+    }
     public LinkedList(Node<T> node)
     {
       Head = node;
@@ -26,6 +28,36 @@ namespace DataStructures
         Tail.Next = node;
         Tail = node;
       }
+    }
+    public void Add(T value)
+    {
+      Node<T> node = new Node<T>(value);
+      if (Head == null)
+      {
+        Head = node;
+      }
+
+      else if (Head.Next == null)
+      {
+        Head.Next = node;
+        Tail = node;
+      }
+      else
+      {
+        Tail.Next = node;
+        Tail = node;
+      }
+    }
+    public void Print()
+    {
+      Console.WriteLine("Linked List:\n==============");
+      Node<T> curNode = Head;
+      while(curNode != null)
+      {
+        Console.Write($"{curNode.Value} -> ");
+        curNode = curNode.Next;
+      }
+      Console.WriteLine("Null");
     }
   }
 }
